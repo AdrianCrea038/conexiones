7a14f56829e0e6fe4e4d3c536fd1eb84b0905add
AdrianCrea038 <adrian.creativo038@gmail.com>
2026-05-03 11:17:55 -0600

    Delete js directory

--- js/script.js
+++
@@ -1,6 +0,0 @@
-const menu = document.querySelector('.menu-toggle');
-const nav = document.querySelector('.nav-links');
-
-menu.addEventListener('click', () => {
-    nav.classList.toggle('active');
-});
